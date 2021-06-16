3110575c125387b740dfbf1d172613c0dddd7f38
Jacob Minock <jaminock@umich.edu>
2021-06-16 18:50:56 -0500

    Generated catkin ws

---
+++ software/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/mission9-2021/software/devel:$CMAKE_PREFIX_PATH"
+export PWD='/mission9-2021/software/build'
+export ROSLISP_PACKAGE_DIRECTORIES='/mission9-2021/software/devel/share/common-lisp'
+export ROS_PACKAGE_PATH="/mission9-2021/software/src:$ROS_PACKAGE_PATH"

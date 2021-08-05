a8268c07f12329346f975ef0d8c05d2081ec24cd
schefferac2020 <schefferandrew66@gmail.com>
2021-08-05 15:01:51 -0400

    Add basic GPS Navigation
    Reads in setpoints from missions/mission.plan and flies waypoints mission. Currently it just crashes after completion. MUST
    change altitudes in .plan files to be in meters above start location insead of meters above sea level for it to work.
    
    The documentation was mostly outdated for this. There was an issue with getting the correct altitude measurements.
    The node can push setpoints to the mavros/setpoint_position/global which takes in lat, lon, alt coords. It can then compare
    its current lat/lon position by subscribing to mavros/global_position/global but I found the altitude returned by that topic
    was drastically different than expected (about 50 meters and it varied based on where you were). Temperary fix was just to
    subscribe to mavros/altitude topic which ended up working. There's probably a better way of doing it and I'm pretty sure the
    discrepency has something to so with the topics having different message types.

--- software/src/navigation_test/src/offboard_node.cpp
+++ software/src/navigation_test/src/offboard_node.cpp
@@ -2,6 +2,7 @@
 
 #include <ros/ros.h>
 #include <geometry_msgs/PoseStamped.h>
+#include <geographic_msgs/GeoPoseStamped.h>
 #include <mavros_msgs/CommandBool.h>
 #include <mavros_msgs/CommandTOL.h>
 #include <mavros_msgs/SetMode.h>
@@ -11,20 +12,44 @@
 #include <mavros_msgs/WaypointPush.h>
 #include <mavros_msgs/Waypoint.h>
 #include <mavros_msgs/CommandVtolTransition.h>
+#include <mavros_msgs/Altitude.h>
+#include <sensor_msgs/NavSatFix.h>
 
-
-#define FLYING_ALTITUDE 1.5f
+#define FLYING_ALTITUDE 2.0f
 
 
 mavros_msgs::State current_state;
 nav_msgs::Odometry current_pose;
-geometry_msgs::PoseStamped current_waypoint;
+geometry_msgs::PoseStamped current_local_waypoint;
+geographic_msgs::GeoPoseStamped current_global_waypoint;
 
 
-void waypt_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
-	current_waypoint = *msg;
+//Initial Position Callbacks
+double latitude, longitude;
+bool gps_recieved = false;
+void get_global_pose_cb(const sensor_msgs::NavSatFix::ConstPtr& msg){
+	latitude = msg->latitude;
+	longitude = msg->longitude;
+	gps_recieved = true;
 }
 
+double altitude;
+bool alt_recieved = false;
+void get_altitude_cb(const mavros_msgs::Altitude::ConstPtr& msg){
+	altitude = msg->amsl;
+	alt_recieved = true;
+}
+
+//Waypoint CallBacks
+void global_waypt_cb(const geographic_msgs::GeoPoseStamped::ConstPtr& msg){
+	current_global_waypoint = *msg;
+}
+
+void local_waypt_cb(const geometry_msgs::PoseStamped::ConstPtr& msg){
+	current_local_waypoint = *msg;
+}
+
+//State callback
 void state_cb(const mavros_msgs::State::ConstPtr& msg){
 	current_state = *msg;
 }
@@ -34,14 +59,18 @@ int main (int argc, char** argv){
 	ros::NodeHandle nh;
 
 	ros::Subscriber state_sub = nh.subscribe<mavros_msgs::State>("mavros/state", 20, state_cb);
-	ros::Subscriber next_wp_sub = nh.subscribe("next_waypoint", 100, waypt_cb);	
+	ros::Subscriber next_loc_wp_sub = nh.subscribe("next_local_waypoint", 100, local_waypt_cb);
+	ros::Subscriber next_global_wp_sub = nh.subscribe("next_global_waypoint", 100, global_waypt_cb);
+	ros::Subscriber sub_global_pose = nh.subscribe<sensor_msgs::NavSatFix>("mavros/global_position/global", 10, get_global_pose_cb);
+	ros::Subscriber sub_altitude = nh.subscribe<mavros_msgs::Altitude>("mavros/altitude", 10, get_altitude_cb);
 
 	ros::Publisher local_pos_pub = nh.advertise<geometry_msgs::PoseStamped>("mavros/setpoint_position/local", 10);
+	ros::Publisher global_pos_pub = nh.advertise<geographic_msgs::GeoPoseStamped>("mavros/setpoint_position/global", 10);
+		
 	ros::ServiceClient arming_client = nh.serviceClient<mavros_msgs::CommandBool>("mavros/cmd/arming");
 	ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
 	ros::ServiceClient set_mode_client = nh.serviceClient<mavros_msgs::SetMode>("mavros/set_mode");
 
-	ros::ServiceClient wp_client = nh.serviceClient<mavros_msgs::WaypointPush>("mavros/mission/push");
 	ros::ServiceClient takeoff_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/takeoff");
 	
 	ros::Rate rate(20.0);
@@ -52,6 +81,19 @@ int main (int argc, char** argv){
         	rate.sleep();
         	ROS_INFO("connecting to FCT...");
    	}
+
+	ROS_INFO("Waiting for Home to be set...");
+	while (ros::ok() && (!gps_recieved || !alt_recieved)){
+    		ros::spinOnce();
+    		rate.sleep();
+  	}
+
+
+	//Set Home Position
+	const double home_lat = latitude;
+	const double home_lon = longitude;
+	const double home_alt = altitude;
+	ROS_INFO("GPS Home Recieved --> lat: %f, lon: %f, alt: %f", home_lat, home_lon, home_alt);
 
     	geometry_msgs::PoseStamped pose;
     	pose.pose.position.x = 0;
@@ -84,7 +126,7 @@ int main (int argc, char** argv){
 	while(ros::ok() && !current_state.armed){
         	if( current_state.mode != "OFFBOARD" &&
             		(ros::Time::now() - last_request > ros::Duration(5.0))){
-          		ROS_INFO(current_state.mode.c_str());
+          		ROS_INFO("%s", current_state.mode.c_str());
             		if( set_mode_client.call(offb_set_mode) &&
                 		offb_set_mode.response.mode_sent){
                 		ROS_INFO("Offboard enabled");
@@ -111,7 +153,7 @@ int main (int argc, char** argv){
     	pose.pose.position.z = FLYING_ALTITUDE;
 
     	ROS_INFO("going to the first way point");
-    	for(int i = 0; ros::ok() && i < 10*20; ++i){
+    	for(int i = 0; ros::ok() && i < 2*10*20; ++i){
     	  local_pos_pub.publish(pose);
     	  ros::spinOnce();
     	  rate.sleep();
@@ -142,7 +184,14 @@ int main (int argc, char** argv){
 
 	//Pull the next waypoints from topic
 	while (ros::ok()){
-    	  	local_pos_pub.publish(current_waypoint);
+    	  	//local_pos_pub.publish(current_local_waypoint);
+		
+
+		geographic_msgs::GeoPoseStamped wp = current_global_waypoint;
+		wp.pose.position.altitude += home_alt;
+		
+		global_pos_pub.publish(wp);
+
 		ros::spinOnce();
 		rate.sleep();
 	}
--- software/src/navigation_test/src/pub_setpoints.cpp
+++ software/src/navigation_test/src/pub_setpoints.cpp
@@ -4,98 +4,265 @@
 #include <vector>
 #include "geometry_msgs/PoseStamped.h"
 #include "geometry_msgs/Vector3Stamped.h"
+#include "geographic_msgs/GeoPoseStamped.h"
 #include "nav_msgs/Odometry.h"
 #include "mavros_msgs/CommandTOL.h"
+#include "sensor_msgs/NavSatFix.h"
+#include "mavros_msgs/Altitude.h"
+#include <math.h>
 
-struct Waypoint{
+
+// Boost for JSON parsing
+#include <boost/bind.hpp>
+//#include <boost/cstdfloat.hpp>
+#include <boost/foreach.hpp>
+#include <boost/property_tree/json_parser.hpp>
+#include <boost/property_tree/ptree.hpp>
+#include <exception>
+#include <iostream>
+#include <set>
+#include <string>
+
+namespace bpt = boost::property_tree;
+
+
+struct Waypoint{ // x, y, z or lat, long, alt depending on what you put in
 	double x, y, z;
 };
 
 class Navigator {
-	public:
-		Navigator(ros::NodeHandle& nh)  {
-			pose_pub = nh.advertise<geometry_msgs::PoseStamped>("next_waypoint", 100);
-		}
+    public:
+	Navigator(ros::NodeHandle& nh)  {
+	    local_pose_pub = nh.advertise<geometry_msgs::PoseStamped>("next_local_waypoint", 100);
+	    global_pose_pub = nh.advertise<geographic_msgs::GeoPoseStamped>("next_global_waypoint", 100);	
+	}
 
-		void pose_callback(const nav_msgs::Odometry::ConstPtr& msg){
-			curr_pose = *msg;
-		}
+	void setGroundAlt(){
+	    ros::Rate rate(20.0);
 
-		void loadWaypoints(std::vector<Waypoint>& wps){
-			this->_waypoints = wps;
-			this->curr_waypoint_ind = 0;
-		}
+            while (ros::ok() && !got_alt){
+	    	ROS_INFO("Wassup my guy");
+		ros::spinOnce();
+		rate.sleep();
+	    }
 
-		void runMission(){			
-			
-			ros::Rate loop_rate(20.0);
-			geometry_msgs::PoseStamped msg;
 
-        		Waypoint curr_wp = _waypoints[curr_waypoint_ind];
-			
-			int c= 0;
-			while (curr_waypoint_ind < _waypoints.size()){
-				if (this->atWaypoint()){
-					ROS_INFO("REACHED waypoint #[%d]", curr_waypoint_ind);
-					curr_wp = _waypoints[++curr_waypoint_ind];
-				}
-				msg.header.seq = c;
-				msg.header.stamp = ros::Time::now();
-				msg.header.frame_id = 1;
-
-				msg.pose.position.x = curr_wp.x;
-				msg.pose.position.y = curr_wp.y;
-				msg.pose.position.z = curr_wp.z;
-			
-				pose_pub.publish(msg);
-
-				ros::spinOnce();
-				loop_rate.sleep();
-				++c;
-			}
-		}
-
-		
-	private:
-		bool atWaypoint(){
-        		Waypoint wp = _waypoints[curr_waypoint_ind];
-			
-			double max_error = 1.0; // This is in meters
-
-        		double diff_x = abs(wp.x - curr_pose.pose.pose.position.x);
-        		double diff_y = abs(wp.y - curr_pose.pose.pose.position.y);
-        		double diff_z = abs(wp.z - curr_pose.pose.pose.position.z);
-
-        		double dist = sqrt(diff_x*diff_x + diff_y*diff_y + diff_z* diff_z);
-
-        		if (dist < max_error){
-                		return true;
-        		}else{
-                		return false;
-        		}
-		}
-		
-
-		ros::Publisher pose_pub;
+	    this->grounded_alt = curr_alt;
+	    ROS_INFO("Grounded alt = %f", this->grounded_alt);
+	}
 	
-		nav_msgs::Odometry curr_pose;
-		std::vector<Waypoint> _waypoints;
-		int curr_waypoint_ind;
+	void local_pose_callback(const nav_msgs::Odometry::ConstPtr& msg){
+	    curr_local_pose = *msg;
+	}
+
+
+	void global_pose_callback(const sensor_msgs::NavSatFix::ConstPtr& msg){
+	    curr_global_pose = *msg;
+	}
+
+	void altitude_callback(const mavros_msgs::Altitude::ConstPtr& msg){
+	    this->curr_alt = msg->amsl;
+	    this->got_alt = true;
+	}
+
+	//Populates local or global waypoints
+	void loadWaypoints(std::vector<Waypoint>& wps, bool isGPS = false){
+	    this->_waypoints = wps;
+	    this->curr_waypoint_ind = 0;
+	    this->isGPS = isGPS;
+	}
+
+	//Populates waypoints with lat/long/alt coords from a mission.plan file from QGC
+	void loadWaypointsFromFile(const std::string filename){
+	    this->isGPS = true;
+	    this->curr_waypoint_ind = 0;
+	    this->_waypoints = {};
+
+	    std::ifstream file(filename);
+	    std::stringstream ss;
+
+	    ss << file.rdbuf();
+	    file.close();
+
+	    bpt::ptree mission_pt;
+	    bpt::read_json(ss, mission_pt);
+
+	    for (auto& mi : mission_pt.get_child("mission.items")){
+		ROS_INFO("Reading Waypoint...");
+		Waypoint wp;
+
+		// Parameters
+		std::vector<double> params;		
+		for (auto& p : mi.second.get_child("params")){
+		    try{
+			params.push_back(p.second.get<double>(""));
+		    }catch(...){ // Throws if there is a null in a param so I just changed it to 0
+			params.push_back(0);
+		    }
+		}
+
+		wp.x = params[4]; // Lat
+		wp.y = params[5]; // Long
+		wp.z = params[6]; // Alt
+		this->_waypoints.push_back(wp);
+    		}
+	}
+
+	//Loop that constantly pushes the next waypoint in the mission to 
+	//a ros topic (either GPS or local)
+	void runMission(){			
+	    ros::Rate loop_rate(20.0);
+
+	    Waypoint curr_wp = _waypoints[curr_waypoint_ind];
+
+	    int c= 0;
+	    while (curr_waypoint_ind < _waypoints.size()){
+		if (this->atWaypoint()){
+		    ROS_INFO("REACHED waypoint #[%d]", curr_waypoint_ind);
+		    printCurrentGPSPosition();
+		    curr_wp = _waypoints[++curr_waypoint_ind];
+		}
+
+		pushWaypoint(curr_wp, c);
+
+		ros::spinOnce();
+		loop_rate.sleep();
+		++c;
+	    }
+	}
+
+
+    private:
+	void printCurrentGPSPosition(){
+		double lat = curr_global_pose.latitude;
+		double lon = curr_global_pose.longitude;
+		double alt = this->curr_alt;
+		ROS_INFO("Currently at position lat: %f, lon: %f, alt: %f", lat, lon, alt);
+	}	
+	//Actually pushes the message to correct topic
+	void pushWaypoint(Waypoint& curr_wp, int c){
+		if (!isGPS){ // if it is local waypoint
+
+	    		geometry_msgs::PoseStamped msg;
+
+			msg.header.seq = c;
+			msg.header.stamp = ros::Time::now();
+			msg.header.frame_id = 1;
+
+			msg.pose.position.x = curr_wp.x;
+			msg.pose.position.y = curr_wp.y;
+			msg.pose.position.z = curr_wp.z;
+
+			local_pose_pub.publish(msg);
+		}else{
 		
+	    		geographic_msgs::GeoPoseStamped msg;
+
+			msg.header.seq = c;
+			msg.header.stamp = ros::Time::now();
+			msg.header.frame_id = 1;
+
+			msg.pose.position.latitude = curr_wp.x; //Lat
+			msg.pose.position.longitude = curr_wp.y; //Lon
+			msg.pose.position.altitude = curr_wp.z; //Alt
+
+			global_pose_pub.publish(msg);
+		}
+	}
+
+
+	bool atWaypoint(){
+		if (isGPS){
+			return atWaypointGlobal();
+		}else{
+			return atWaypointLocal();
+		}
+	}
+
+	//Great Circle dist using Haversine formula then pythag
+	bool atWaypointGlobal(){
+		Waypoint wp = _waypoints[curr_waypoint_ind];
+		
+		double max_error = 3; //Maybe this should change depending on how sharp of a turn it has to do? idk
+		
+		//Haversine formula
+		double lat1 = wp.x; double lon1 = wp.y;
+		double lat2 = curr_global_pose.latitude; double lon2 = curr_global_pose.longitude;
+
+		double dLat = (lat2 - lat1) * M_PI / 180.0;
+		double dLon = (lon2 - lon1) * M_PI / 180.0;
+
+		lat1 = (lat1) * M_PI / 180.0;
+		lat2 = (lat2) * M_PI / 180.0;
+
+		double a = pow(sin(dLat / 2), 2) + pow(sin(dLon / 2), 2) * cos(lat1) * cos(lat2);
+		double rad = 6371 * 1000;
+		double c = 2*asin(sqrt(a));
+		double flat_dist = rad * c; // in M
+
+		//Pythag
+		double targetAlt = this->grounded_alt + wp.z;
+		double dAlt = abs(targetAlt - this->curr_alt);
+		
+		double dist = sqrt(flat_dist*flat_dist + dAlt*dAlt);
+		
+		ROS_INFO("Distance to next: %f", dist);
+		if (dist < max_error){
+			return true;
+		}else{
+			return false;
+		}
+	}
+	
+	//Simple Pythag Distance
+	bool atWaypointLocal(){
+	    Waypoint wp = _waypoints[curr_waypoint_ind];
+
+	    double max_error = 1.0; // This is in meters
+
+	    double diff_x = abs(wp.x - curr_local_pose.pose.pose.position.x);
+	    double diff_y = abs(wp.y - curr_local_pose.pose.pose.position.y);
+	    double diff_z = abs(wp.z - curr_local_pose.pose.pose.position.z);
+
+	    double dist = sqrt(diff_x*diff_x + diff_y*diff_y + diff_z* diff_z);
+
+	    if (dist < max_error){
+		return true;
+	    }else{
+		return false;
+	    }
+	}
+
+
+	bool isGPS;
+	bool got_alt = false;
+
+	ros::Publisher local_pose_pub;
+	ros::Publisher global_pose_pub;
+
+	nav_msgs::Odometry curr_local_pose;
+	double  grounded_alt;
+	double curr_alt;
+	sensor_msgs::NavSatFix curr_global_pose;	
+	std::vector<Waypoint> _waypoints;
+	int curr_waypoint_ind;
 };
 
 int main(int argc, char** argv){
 	ros::init(argc, argv, "waypoint_publisher");
 	ros::NodeHandle nh;
 
-	
-
-
 	Navigator navigator(nh);
-	ros::Subscriber current_pos = nh.subscribe("/mavros/global_position/local", 20, &Navigator::pose_callback, &navigator);
+	ros::Subscriber current_local_pos = nh.subscribe("/mavros/global_position/local", 20, &Navigator::local_pose_callback, &navigator);	
+	ros::Subscriber current_global_pos = nh.subscribe("/mavros/global_position/global", 20, &Navigator::global_pose_callback, &navigator); 
+	ros::Subscriber altitude_sub = nh.subscribe("/mavros/altitude", 20, &Navigator::altitude_callback, &navigator);
+	ROS_INFO("Here");
+	navigator.setGroundAlt();
+	//DEFINE THE MISSION BELOW
 	
-	//ros::ServiceClient land_client = nh.serviceClient<mavros_msgs::CommandTOL>("mavros/cmd/land");
- 
+
+	//For a local mission
+	/*
 	//Load the waypoints;	
 	ROS_INFO("LOADING WAYPOINTS...");
 	std::vector<Waypoint> waypoints = {{100, 100, 20}, {0, 100, 20}, {200, 0, 20}};
@@ -107,32 +274,20 @@ int main(int argc, char** argv){
 	//Run the waypoint mission
 	ROS_INFO("MISSION IN PROGRESS...");
 	navigator.runMission();	
+	*/
 
+	
+	//For a GPS Mission
+	ROS_INFO("LOADING THE WAYPOINTS...");
+	navigator.loadWaypointsFromFile("/mission9-2021/software/src/navigation_test/missions/first_mission.plan");
+	ROS_INFO("SUCCESS: LOADED THE WAYPOINTS!!!");
 
+	ROS_INFO("MISSION IN PROGRESS...");
+	navigator.runMission();
 
 	ROS_INFO("FINISHED!!!");
-
-
-	/*while (ros::ok()){
-		msg.header.seq = c;
-		msg.header.stamp = ros::Time::now();
-		msg.header.frame_id = 1;
-
-		msg.pose.position.x = 0.0;
-		msg.pose.position.y = 0.0;
-		msg.pose.position.z = 1.5;
-
-		msg.pose.orientation.x = 0;
-		msg.pose.orientation.y = 0;
-		msg.pose.orientation.z = 2;
-		msg.pose.orientation.w = 1;
-	
-		pose_pub.publish(msg);
-		ros::spinOnce();
-		++c;
-		loop_rate.sleep();
-	}*/
 
 	return 0;
 
 }
+

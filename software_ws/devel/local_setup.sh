f5ceb46284759530660460046d8dbb965c12e1e5
Drew Scheffer <drewskis@umich.edu>
2022-09-08 15:12:27 -0400

    Created new software_ws catkin workspace

--- software_ws/devel/local_setup.sh
+++ software_ws/devel/local_setup.sh
@@ -3,7 +3,7 @@
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/mission9-2021/software/devel}
+: ${_CATKIN_SETUP_DIR:=/mission9-2022/software_ws/devel}
 CATKIN_SETUP_UTIL_ARGS="--extend --local"
 . "$_CATKIN_SETUP_DIR/setup.sh"
 unset CATKIN_SETUP_UTIL_ARGS
